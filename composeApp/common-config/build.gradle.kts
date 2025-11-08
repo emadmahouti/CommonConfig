246870884af8e477b0b117cf6c1ea3fe413f1a00
Emad Mahouti <e.mahouti@nizek.com>
2025-11-08 22:55:37 +0330

    Published

--- composeApp/common-config/build.gradle.kts
+++ composeApp/common-config/build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
     `kotlin-dsl`
     `java-gradle-plugin`
+    id("com.gradle.plugin-publish") version "1.3.0"
 }
+
+group = "io.github.emadmahouti.kmp"
+version = "1.0.0"
 
 dependencies {
     implementation("com.squareup:kotlinpoet:1.14.2")
@@ -11,11 +15,13 @@ dependencies {
 gradlePlugin {
     plugins {
         create("configPlugin") {
-            implementationClass = "io.github.emadmahouti.kmp.CommonConfig"
-            displayName = ""
-            description = ""
-            tags = listOf("")
             id = "io.github.emadmahouti.kmp.commonConfig"
+            implementationClass = "io.github.emadmahouti.kmp.CommonConfig"
+            displayName = "CommonConfig Plugin"
+            description = "Gradle plugin for Kotlin Multiplatform projects to generate type-safe build constants."
+            tags = listOf("kmp", "cmp", "kotlinMultiplatform", "buildConfig")
         }
     }
+    website = "https://github.com/emadmahouti/CommonConfig"
+    vcsUrl = "https://github.com/emadmahouti/CommonConfig"
 }
